955e35397087539effc08f316acf5cbe9d8e046a
Filip Procházka (HosipLan) <HosipLan@gmail.com>
2010-09-07 11:11:14 +0200

    bootstrap typo

--- phpQuery/bootstrap.php
+++ phpQuery/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * Example of phpQuery bootstrap file.
+ * phpQuery bootstrap file.
  *
  * This file is executed everytime phpQuery is included. Use it to set all
  * your personal needs in the library.
- *
- * To activate this file, delete '.example' from filename.
  */
 
 // probably you want to use one of those functions here
